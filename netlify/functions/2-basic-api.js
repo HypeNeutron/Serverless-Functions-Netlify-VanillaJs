cb3255d5bc4fb7e4c69119922a352ffd91ee8601
Oam surachet <surachat2014@gmail.com>
2021-05-07 13:05:05 +0700

    fix access origin

--- netlify/functions/2-basic-api.js
+++ netlify/functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../../assets/data');
 // const items = [{ name: 'susan' }, { name: 'john Doe' }];
 
 exports.handler = async (event, context) => ({
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
   statusCode: 200,
   body: JSON.stringify(items),
 });
